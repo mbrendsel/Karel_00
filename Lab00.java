e13680845be40358331da98018a2c438426d522f
mbrendsel <monica_brendsel@asl.org>
2014-11-21 10:23:37 +0000

    Update Lab00.java

--- Lab00.java
+++ Lab00.java
@@ -14,9 +14,17 @@ public class Lab00 {
      public static void main(String[] args) {
              Display.openWorld("maps/first.map");
    Display.setSize(10, 10);
-   Display.setSpeed(3);
+   Display.setSpeed(5);
    Robot karel = new Robot();
    karel.move();
+   karel.pickBeeper();
    karel.move();
+   karel.turnLeft();
+   karel.move();
+   karel.putBeeper();
+   karel.move();
+   karel.turnLeft();
+   karel.turnLeft();
+   
      }
 }
